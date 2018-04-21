9fef0310a44bffbf7e7a77d69cd463fabc695597
kishorssk <kishor.sathigrama@gmail.com>
2018-04-21 16:49:04 +0530

    base url updates

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BASE_URL', 'http://192.168.1.42/tungalahari/');
+define('BASE_URL', 'http://localhost/tungalahari/');
 define('PUBLIC_URL', BASE_URL . 'public/');
 define('XML_SRC_URL', BASE_URL . 'md-src/xml/');
 define('PHOTO_URL', PUBLIC_URL . 'Awardees/');
